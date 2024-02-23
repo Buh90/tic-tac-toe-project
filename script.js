33b28ea910d3cbe8ff07929c58b153a6cc923c08
Buh90 <83811198+Buh90@users.noreply.github.com>
2024-02-23 19:56:36 +0100

    Add JS features

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const initGame = (function () {
+const game = (function () {
   //DOM selectors
   const aiModeBtn = document.querySelector("#AI-mode");
   const friendModeBtn = document.querySelector("#friend-mode");
   const startGameBtn = document.querySelector("#start-game");
   const gameField = document.querySelector(".game-field");
   const gameGrid = document.querySelector(".game-grid");
+  let gameMode;
 
   //Functions
-  startGameBtn.addEventListener("click", startGame);
+  startGameBtn.addEventListener("click", openField);
 
-  function startGame() {
-    //aiModeBtn.checked === true ? console.log("AI") : console.log("Friend");
+  function openField() {
+    friendModeBtn.setAttribute("disabled", true);
+    aiModeBtn.setAttribute("disabled", true);
+    startGameBtn.setAttribute("disabled", true);
+    aiModeBtn.checked === true
+      ? (game.gameMode = "AI")
+      : (game.gameMode = "friend");
     gameField.classList.add("open");
     if (gameGrid.innerHTML === "") {
       for (let i = 1; i < 10; i++) {
@@ -20,31 +26,76 @@ const initGame = (function () {
         gameGrid.appendChild(card);
       }
     }
-
-    playGame();
+    initGame();
   }
-  return { gameGrid };
+  return { gameGrid, gameMode };
 })();
 
-function playGame() {
+function initGame() {
   let checkerArray = [null, null, null, null, null, null, null, null, null];
   let turnChecker = 1;
-  let cells = Array.from(initGame.gameGrid.childNodes);
+  let cells = Array.from(game.gameGrid.childNodes);
+
   for (cell of cells) {
-    cell.addEventListener(
-      "click",
-      function () {
-        if (turnChecker % 2 === 1) {
-          this.classList.add("x");
-          checkerArray[this.getAttribute("data-card") - 1] = "x";
-        } else {
-          this.classList.add("o");
-          checkerArray[this.getAttribute("data-card") - 1] = "o";
+    cell.addEventListener("click", function eventHandler() {
+      if (turnChecker % 2 === 1) {
+        this.classList.add("x");
+        checkerArray[this.getAttribute("data-card") - 1] = "x";
+        if (checkWinner(checkerArray, cells)) {
+          return;
         }
         turnChecker++;
-        console.log(checkerArray);
-      },
-      { once: true }
-    );
+        if (game.gameMode === "AI" && turnChecker < 9) {
+          playComputerTurn(checkerArray, cells);
+        }
+      } else {
+        this.classList.add("o");
+        checkerArray[this.getAttribute("data-card") - 1] = "o";
+        if (checkWinner(checkerArray, cells)) {
+          return;
+        }
+        turnChecker++;
+      }
+      this.removeEventListener("click", eventHandler);
+    });
   }
+}
+
+function playComputerTurn(checkerArray, cells) {
+  let computerChoice = Math.floor(Math.random() * 9);
+  while (checkerArray[computerChoice] !== null) {
+    computerChoice = Math.floor(Math.random() * 9);
+  }
+  cells[computerChoice].click();
+
+  return checkerArray;
+}
+
+function checkWinner(checkerArray, cells) {
+  let isWinner = false;
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (combination of winningCombinations) {
+    if (
+      checkerArray[combination[0]] === checkerArray[combination[1]] &&
+      checkerArray[combination[1]] === checkerArray[combination[2]] &&
+      checkerArray[combination[0]] !== null
+    ) {
+      isWinner = true;
+      game.gameGrid.setAttribute("inert", true);
+      for (let i = 0; i < 3; i++) {
+        cells[combination[i]].classList.add("winner");
+      }
+      break;
+    }
+  }
+  return isWinner;
 }
